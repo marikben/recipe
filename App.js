96e013893ea32f90d140e995be62654ef38819f6
marikben <70907667+marikben@users.noreply.github.com>
2021-02-14 17:47:18 +0200

    updates

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [desc, setDesc] = useState('');
+  const [recipes, setRecipes] = useState('');
+
+  const getRecipes = async () => {
+    const url = 'http://www.recipepuppy.com/api/?i='+desc
+    
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setRecipes(data.results);
+    } catch(error) {
+    Alert.alert('Error', error);
+    };
+    }
+    const listSeparator = () => {
+      return (
+        <View
+          style={{
+            height: 1,
+            width: "80%",
+            backgroundColor: "#CED0CE",
+            marginLeft: "10%"
+          }}
+        />
+      );
+    };
+
+    const emptyList = () => {
+      return (
+        <Text>No recipes found, please enter a new search</Text>
+      )
+    }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <FlatList
+        style={{marginLeft : "5%"}}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => 
+        <View>
+        <Image style={{width: 40, height: 40}} source={{uri: `${item.thumbnail}`}}/>
+        <Text>{item.title}</Text>
+        </View>
+        }
+        ItemSeparatorComponent={listSeparator}
+        ListEmptyComponent={emptyList}
+        data={recipes}
+        />
+      <TextInput style={{fontSize: 18, width: 200}} value={desc}
+      placeholder="Search term" onChangeText={desc => setDesc(desc)}/>
+      <Button title="Find" onPress={getRecipes} />
       <StatusBar style="auto" />
     </View>
   );
